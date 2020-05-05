c44374d948dd3c55e76cc7574c96eef1e49a99d3
carloserocha <carloserocha27@gmail.com>
2020-05-04 21:39:21 -0300

    Trocando teste de 5 para 6 para evitar confusões no test

--- roman_numerals/test/roman_numerals_test.exs
+++ roman_numerals/test/roman_numerals_test.exs
@@ -2,8 +2,8 @@ defmodule RomanTest do
   use ExUnit.Case
   doctest Roman
 
-  test "convert number to roman numbers 5" do
-    assert Roman.numerals(5) == "V"
+  test "convert number to roman numbers 6" do
+    assert Roman.numerals(6) == "VI"
   end
   test "convert number to roman numbers 15" do
     assert Roman.numerals(15) == "XV"

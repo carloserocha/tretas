ed9f48bc5e0140303e0fd2d3b5ff3b74a2378091
carloserocha <carloserocha27@gmail.com>
2020-06-12 22:17:36 -0300

    Página e Bot do zapzap

--- Tracking/tracking_app/src/TrackingTable.js
+++ Tracking/tracking_app/src/TrackingTable.js
@@ -1,8 +1,39 @@
 import React from 'react';
-import { Table } from 'semantic-ui-react'
+import { Table, Form, Grid } from 'semantic-ui-react'
 import request from 'superagent'
 
 const URI = 'http://192.168.15.70:8000/rastreio/correios'
+
+/*const levels = [
+  {
+    'word': 'postado',
+    'level': 1,
+    'nodes': [],
+    'path': './../public/images/enviada.png'
+  },
+  {
+    'word': 'encaminhado',
+    'level': 1,
+    'nodes': [],
+    'path': './../public/images/enviada.png'
+  },
+  {
+    'word': 'saiu',
+    'level': 2,
+    'nodes': [
+      1
+    ],
+    'path': './../public/images/saiu.png'
+  },
+  {
+    'word': 'entregue',
+    'level': 3,
+    'nodes': [
+      2, 1
+    ],
+    'path': './../public/images/entregue.png'
+  },
+]*/
 
 
 class TrackingTable extends React.Component {
@@ -11,28 +42,32 @@ class TrackingTable extends React.Component {
 
     // fazer os blocos de alerta
     this.state = {
+      tracking_code: '',
       done: false,
       items: [],
-      levels: [
-        {
-          'word' : 'saiu',
-          'level' : 7
-        }
-      ]
+      level: {}
     }
   }
 
-  // TODO: arrumar essa gambiarra para os resultados
-  componentDidMount() {
-    request
-        .get(`${URI}/OA117280554BR`)
-        .then(res => this.setState({items: (JSON.parse(res.text)).results, done: true}))
+  // OA117280554BR
+  /*componentDidMount() {
+   
+  }*/
+
+  myChangeHandler = (event) => {
+    this.setState({ tracking_code: event.target.value });
+    if (this.state.tracking_code.length > 11) {
+      this.setState({ tracking_code: event.target.value });
+      request
+        .get(`${URI}/${event.target.value}`)
+        .then(res => this.setState({ items: (JSON.parse(res.text)).results, done: true }))
         .catch(err => console.log(err))
-}
+    }
+  }
 
   renderTableData() {
     if (this.state.done) {
-      return this.state.items.map((element, index) => {
+      return this.state.items.map((element) => {
         return (
           <Table.Row>
             <Table.Cell>{element.date} {element.hour}</Table.Cell>
@@ -46,16 +81,31 @@ class TrackingTable extends React.Component {
     }
   }
 
+  /*componentImage() {
+    const item = this.state.items[0] // já retorna ordenado
+    const c = levels.filter(i => item.notice.includes(i.word))
+    console.log(c)
+    this.setState({
+      level: c
+    })
+  }*/
+
   render() {
-    if (!this.state.done) {
-      return (
-        <div>
-          Loading...
-        </div>
-      )
-    } else {
-      return (
-        <Table padded>
+    return (
+      <Grid centered columns={3}>
+        <Grid.Column>
+        <Form onSubmit={this.mySubmitHandler} padded>
+        <h3>{this.state.tracking_code}</h3>
+          <Form.Group>
+            <Form.Input
+              placeholder='Insire o código de rastreio'
+              type='text'
+              value={this.state.tracking_code}
+              onChange={this.myChangeHandler}
+            />
+          </Form.Group>
+        </Form>
+        <Table>
           <Table.Header>
             <Table.Row>
               <Table.HeaderCell>Hora</Table.HeaderCell>
@@ -63,13 +113,14 @@ class TrackingTable extends React.Component {
               <Table.HeaderCell>Observação</Table.HeaderCell>
             </Table.Row>
           </Table.Header>
-  
+
           <Table.Body>
             {this.renderTableData()}
           </Table.Body>
         </Table>
-      )
-    }
+        </Grid.Column>
+      </Grid>
+    )
   }
 }
 
